599ef9d6ef9fc6a0cc5c06c07eb04ee11988775a
Johnathan <25joper04@mdusd.net>
2024-02-22 11:06:13 -0800

    Fixed changes for vision

--- robotcontainer.py
+++ robotcontainer.py
@@ -24,7 +24,6 @@ import Subsystems.photonVisionSubsystem
 from Commands.robotLEDsCommand import ledMode1, ledMode2, ledMode3
 from Commands.shooterCommand import inwardsShooter, outwardsShooter, stopShooter
 from Commands.intakeCommand import intake, outake, stopIntake
-from Commands.aprilTagCommand import getAprilTags
 
 
 class RobotContainer:
@@ -42,7 +41,7 @@ class RobotContainer:
         and commands.
         """
         # The robot's subsystems
-        self.arm = Subsystems.shooterSubsystem.shooterSubsystem()
+        self.shooter = Subsystems.shooterSubsystem.shooterSubsystem()
         self.led = Subsystems.robotLEDsSubsystem.robotLEDsSubsystem()
         self.intake = Subsystems.intakeSubsystem.intakeSubsystem()
         self.Vision = Subsystems.photonVisionSubsystem.visionSub()
@@ -67,11 +66,11 @@ class RobotContainer:
         # self.Vision.setDefaultCommand(self.Vision.periodic())
         
         
-        self.stick.leftBumper().whileTrue(inwardsShooter(self.arm))
-        self.stick.leftBumper().whileFalse(stopShooter(self.arm, self.led))
+        self.stick.leftBumper().whileTrue(inwardsShooter(self.shooter))
+        self.stick.leftBumper().whileFalse(stopShooter(self.shooter, self.led))
         
-        self.stick.rightBumper().whileTrue(outwardsShooter(self.arm, self.led))
-        self.stick.rightBumper().whileFalse(stopShooter(self.arm, self.led))
+        self.stick.rightBumper().whileTrue(outwardsShooter(self.shooter, self.led))
+        self.stick.rightBumper().whileFalse(stopShooter(self.shooter, self.led))
         
         self.stick.button(2).whileTrue(outake(self.intake))
         self.stick.button(2).whileFalse(stopIntake(self.intake, self.led))
--- subsystems/photonVisionSubsystem.py
+++ subsystems/photonVisionSubsystem.py
@@ -3,11 +3,11 @@ import commands2
 
 from photonlibpy import estimatedRobotPose
 from photonlibpy import multiTargetPNPResult
-from photonlibpy import photonCamera
-from photonlibpy import photonPipelineResult
+from photonlibpy.photonCamera import PhotonCamera
+from photonlibpy.photonPipelineResult import PhotonPipelineResult
 from photonlibpy import packet
 from photonlibpy import photonPoseEstimator
-from photonlibpy import photonTrackedTarget
+from photonlibpy.photonTrackedTarget import PhotonTrackedTarget
 
 from constants import OP
 
@@ -18,55 +18,14 @@ class visionSub(commands2.Subsystem):
     def  __init__(self):
         
         # Start camera server 
-        self.camera = photonCamera.PhotonCamera("limelight") 
+        self.camera = PhotonCamera("limelight")
+        
+        
+        
+    def teleopPeriodic(self):
+        
         self.camResult = self.camera.getLatestResult()
-        # self.targets = photonPipelineResult.PhotonPipelineResult.getTargets()
-        self.target = photonTrackedTarget.PhotonTrackedTarget
+        self.pipeline = self.camResult.getTargets()
         
-        
-        
-        # initiate smartDashboard
-        self.smartDashboard = wpilib.SmartDashboard
-        
-        # self.smartDashboard.putNumberArray("X, Y, and Z Values for AprilTag", self.target.getBestCameraToTarget)
-        # self.smartDashboard.putNumber("X, Y, and Z Values for AprilTag", self.target.getBestCameraToTarget)
-        
-        
-        
-    def pereodic(self):
-        
-        self.aprilTag = photonTrackedTarget.PhotonTrackedTarget.getFiducialId
-        self.apriltagID = 0
-        
-        if self.aprilTag == 1:
-            self.apriltagID = 1
-        elif self.aprilTag == 2:
-            self.apriltagID = 2
-        elif self.aprilTag == 3:
-            self.apriltagID = 3
-        elif self.aprilTag == 4:
-            self.apriltagID = 4
-        elif self.aprilTag == 5:
-            self.apriltagID = 5 
-        elif self.aprilTag == 6:
-            self.apriltagID = 6
-        elif self.aprilTag == 7:
-            self.apriltagID = 7
-        elif self.aprilTag == 8:
-            self.apriltagID = 8
-        elif self.aprilTag == 9:
-            self.apriltagID = 9
-        elif self.aprilTag == 10:
-            self.apriltagID = 10
-        elif self.aprilTag == 11:
-            self.apriltagID = 11
-        elif self.aprilTag == 12:
-            self.apriltagID = 12
-        elif self.aprilTag == 13:
-            self.apriltagID = 13
-        
-        
-        self.smartDashboard.putNumber("April Tag ID", self.apriltagID)
-        
-    def nothing(self):
-        self.apriltagID = 0
+        self.fidicial_ids= [ i.getFiducialId() for i in self.pipeline ]
+        wpilib.SmartDashboard.putNumberArray("April Tag ID", self.fidicial_ids)
