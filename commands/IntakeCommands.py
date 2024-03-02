215b561a2d9e1f2f8b3f144f4320d8c50be3c59c
Fabian Campos Hernandez <25facam01@mdusd.net>
2024-03-01 16:53:03 -0800

    added intake command

---
+++ commands/IntakeCommands.py
@@ -0,0 +1,112 @@
+import wpilib
+import rev
+import commands2
+import commands2.cmd
+
+from subsystems.IntakeSubsystem import IntakeSubsystem
+import constants
+ 
+class IntakeLimitCommand(commands2.Command):
+    
+    def __init__(self, intakeSubsystem: IntakeSubsystem) -> None:
+        super().__init__()
+        self.intakeSub = intakeSubsystem
+
+    def intialize(self):
+        import logging
+        logger = logging.getLogger("fabian campooos hernandez")
+        logger.info("Intakie")
+
+        
+    def execute(self):
+        self.intakeSub.intake()
+
+    def isFinished(self):
+        return self.intakeSub.limit_switch_get_none()
+    
+    def end(self, interrupted: bool):
+        self.intakeSub.stopIntake()
+
+class StopIntakeLimit(commands2.Command):
+    
+    def __init__(self, intakeSubsystem: IntakeSubsystem) -> None:
+        super().__init__()
+        self.intakeSub = intakeSubsystem
+
+    def intialize(self):
+        import logging
+        logger = logging.getLogger("fabian campooos hernandez")
+        logger.info("stop Intakie")
+        
+    def execute(self):
+        self.intakeSub.stopIntake()
+
+    def isFinished(self):
+        return False
+     
+    def end(self, interrupted: bool):
+        self.intakeSub.stopIntake()
+
+class IntakeCommand(commands2.Command):
+    
+    def __init__(self, intakeSubsystem: IntakeSubsystem) -> None:
+        super().__init__()
+        self.intakeSub = intakeSubsystem
+
+    def intialize(self):
+        import logging
+        logger = logging.getLogger("fabian campooos hernandez")
+        logger.info("Intakie")
+
+        
+    def execute(self):
+        self.intakeSub.intake()
+
+    def isFinished(self):
+        return False
+    
+    def end(self, interrupted: bool):
+        self.intakeSub.stopIntake()
+
+class StopIntake(commands2.Command):
+    
+    def __init__(self, intakeSubsystem: IntakeSubsystem) -> None:
+        super().__init__()
+        self.intakeSub = intakeSubsystem
+
+    def intialize(self):
+        import logging
+        logger = logging.getLogger("fabian campooos hernandez")
+        logger.info("stop Intakie")
+        
+    def execute(self):
+        self.intakeSub.stopIntake()
+
+    def isFinished(self):
+        return False
+     
+    def end(self, interrupted: bool):
+        self.intakeSub.stopIntake()
+
+
+
+class ReverseIntake(commands2.Command):
+    
+    def __init__(self, intakeSubsystem: IntakeSubsystem) -> None:
+        super().__init__()
+        self.intakeSub = intakeSubsystem
+    
+    def initialize(self):
+        import logging
+        logger = logging.getLogger("fabian campooos hernandez")
+        logger.info("reverse intake")
+
+    def execute(self):
+        self.intakeSub.reverseIntake()
+
+    def isFinished(self):
+       return False
+    
+    def end(self, interrupted: bool):
+        self.intakeSub.stopIntake()
+
