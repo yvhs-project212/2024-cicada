0a73abae8996ded21e0e4e6cbbffea94be00aa5c
Gael <kingfatfish123@gmail.com>
2024-03-04 16:25:32 -0800

    new code with limit switch

--- subsystems/intakesubsystem.py
+++ subsystems/intakesubsystem.py
@@ -5,9 +5,11 @@ from constants import ELEC
 import phoenix5
 
 class intake_ss(commands2.Subsystem):
-        def __init__(self) -> None:
+        def __init__(self) -> bool:
             super().__init__()
-            self.motor = phoenix5.WPI_TalonSRX(ELEC.intake_CAN_ID)
+            self.motor = rev.CANSparkMax(rev.CANSparkMax.MotorType.kBrushless)
+            #self.motor = phoenix5.WPI_TalonSRX(ELEC.intake_CAN_ID)
+            self.limitswitch = wpilib.DigitalInput(7)
 
         def intake(self):
             self.motor.set(0.3)
@@ -16,4 +18,15 @@ class intake_ss(commands2.Subsystem):
              self.motor.set(-0.3)
 
         def stop(self):
-             self.motor.set(0)
+             if self.limit_switch == 0:
+                self.motor.set(0)
+            
+        def limit_switch(self) -> bool:
+         if self.limitswitch():
+             return False
+         else:
+             return True
+
+
+
+    
